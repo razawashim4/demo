5a34c29cedfea4d7d79b31f7081d18360ae966d9
unknown <you@example.comrazawashim4@gmail.com>
2024-10-20 16:50:03 +0530

    test

---
+++ CheckArmStrong/Program.cs
@@ -0,0 +1,23 @@
+﻿CheckPalindrom();
+void CheckPalindrom()
+{
+    Console.WriteLine("Enter a number or sentance");
+    string? num=Console.ReadLine();
+    if(num != null)
+    for(int i = 0, j = num.Length - 1; i < num.Length / 2; i++, j--)
+    {
+            if (num[i] == num[j])
+            {
+                if(i == ((num.Length/2)-1))
+                {
+                    Console.WriteLine("Your number is Armstrong");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your number is  not Armstrong");
+                break;
+            }
+    }
+
+}
---
+++ IntersectionOfTwoArray/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("HWe are finding the Intersection of int Array");
+
+FindIntersectioOfTwoIntArray();
+void FindIntersectioOfTwoIntArray()
+{
+    Console.WriteLine("Enter your First Int array ");
+    int[] a = [1, 2, 3, 4, 5];
+    int[] b = [1, 2, 4, 7, 8, 9];
+    var c=a.Intersect(b);
+    Console.WriteLine(string.Join(",",c));
+}
